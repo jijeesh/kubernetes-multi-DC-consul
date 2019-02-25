9db33b593f287932c036b0dc12535e8bf89d1431
Jijeesh <jijeesh.ka@gmail.com>
2019-02-25 23:05:15 +0530

    created first steps to the multi data center

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # kubernetes-multi-DC-consul
 Kubernetes multi data centre with consul
+
+
+### Prerequisites
+
+Install:
+
+1. [Go](https://golang.org/doc/install)
+1. CloudFlare's [SSL ToolKit](https://github.com/cloudflare/cfssl) (`cfssl` and `cfssljson`)
+1. [Consul](https://www.consul.io/docs/install/index.html)
+1. [Vault](https://www.vaultproject.io/docs/install/)
+
+Once installed, create a workspace, configure the GOPATH and add the workspace's bin folder to your system path:
+```
+$ mkdir $HOME/go
+$ export GOPATH=$HOME/go
+$ export PATH=$PATH:$GOPATH/bin
+```
+Next, install the SSL ToolKit:
+
+```
+$ go get -u github.com/cloudflare/cfssl/cmd/cfssl
+$ go get -u github.com/cloudflare/cfssl/cmd/cfssljson
+```
+
+### TLS Certificates
+
+Create a Certificate Authority:
+
+```sh
+$ cfssl gencert -initca certs/config/ca-csr.json | cfssljson -bare certs/ca
+```
+
+Create the private keys and TLS certificates:
+
+```sh
+$ cfssl gencert \
+    -ca=certs/ca.pem \
+    -ca-key=certs/ca-key.pem \
+    -config=certs/config/ca-config.json \
+    -profile=default \
+    certs/config/consul-csr.json | cfssljson -bare certs/consul
+```
+You should now see the following PEM files within the "certs" directory:
+
+```
+ca-key.pem
+ca.pem
+consul-key.pem
+consul.pem
+
+```
---
+++ create.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+
+echo "Generating the Gossip encryption key..."
+
+export GOSSIP_ENCRYPTION_KEY=$(consul keygen)
+
+
+echo "Creating the Consul Secret to store the Gossip key and the TLS certificates..."
+
+kubectl -n consul create secret generic consul \
+  --from-literal="gossip-encryption-key=${GOSSIP_ENCRYPTION_KEY}" \
+  --from-file=certs/ca.pem \
+  --from-file=certs/consul.pem \
+  --from-file=certs/consul-key.pem
+
+
+echo "Storing the Consul config in a ConfigMap..."
+
+kubectl -n consul create configmap consul --from-file=config.json
+
+
+echo "Creating the Consul Service..."
+
+kubectl -n consul create -f service.yaml
+
+echo "Create the Consul Service Account .."
+
+kubectl -n consul create -f service-accounts.yaml
+
+echo "Create the Consul Cluster roles .."
+
+kubectl -n consul create -f cluster-roles.yaml
+
+echo "Creating the Consul StatefulSet..."
+
+kubectl -n consul create -f statefulset.yaml
+
+
+echo "Creating a Secret to store the Vault TLS certificates..."
+
+kubectl -n consul create secret generic vault \
+    --from-file=certs/ca.pem \
+    --from-file=certs/vault.pem \
+    --from-file=certs/vault-key.pem
+
+
+echo "Storing the Vault config in a ConfigMap..."
+
+kubectl -n consul create configmap vault --from-file=vault/config.json
+
+
+echo "Creating the Vault Service..."
+
+kubectl -n consul create -f vault/service.yaml
+
+
+echo "Creating the Vault Deployment..."
+
+kubectl -n consul apply -f vault/deployment.yaml
+
+
+echo "All done! Forwarding port 8200..."
+
+POD=$(kubectl -n consul get pods -o=name | grep vault | sed "s/^.\{4\}//")
+
+while true; do
+  STATUS=$(kubectl -n consul get pods ${POD} -o jsonpath="{.status.phase}")
+  if [ "$STATUS" == "Running" ]; then
+    break
+  else
+    echo "Pod status is: ${STATUS}"
+    sleep 5
+  fi
+done
+
+kubectl -n consul port-forward $POD 8200:8200
